4d4be6a19260ebcc041f3bd3d057b06b35714582
Minh Vương <hardingadonis@gmail.com>
2024-07-08 23:11:04 +0700

    [Add] Add `handle_get_info` method in server

--- server/server.cpp
+++ server/server.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
 #include <httplib.h>
 
+#define SERVER "localhost"
+#define PORT 8080
+#define FILE_FOLDER "files/"
+
+void handle_get_info(const httplib::Request& req, httplib::Response& res)
+{
+	const std::string file_name =  req.get_param_value("file");
+	const std::string file_path = FILE_FOLDER + file_name;
+
+	std::cout << "GET /info - " << file_name << std::endl;
+
+	std::ifstream file(file_path, std::ios::binary);
+
+	if (!file.is_open())
+	{
+		std::cerr << "File not found: " << file_name << std::endl;
+
+		res.status = 404;
+		res.set_content("File not found", "text/plain");
+		return;
+	}
+
+	int content_length = 0;
+	char c;
+	while (file.get(c))
+	{
+		content_length++;
+	}
+
+	file.close();
+
+	res.set_content("", "");
+	res.set_header("Accept-Ranges", "bytes");
+	res.set_header("Content-Length", std::to_string(content_length));
+	res.set_header("Content-Type", "application/octet-stream");
+
+	std::cout << "Content-Length: " << content_length << std::endl;
+}
+
 int main()
 {
+	httplib::Server svr;
+
+	svr.Get("/info", handle_get_info);
+
+	svr.listen(SERVER, PORT);
+
     return EXIT_SUCCESS;
 }
