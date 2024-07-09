3e47635b0aa0c0fb0072699f56ad9fb36239838b
Minh Vương <hardingadonis@gmail.com>
2024-07-09 10:41:26 +0700

    [Add] Add `get_content_length` method in client

--- client/client.cpp
+++ client/client.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 #include <httplib.h>
 
-int main()
+int get_content_length(httplib::Client& cli, const std::string& file_name)
 {
+    auto res = cli.Head("/info?file=" + file_name);
+
+    return std::atoi(res->get_header_value("Content-Length").c_str());
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        std::cout << "Usage: " << argv[0] << " <file_name>" << std::endl;
+
+        return EXIT_FAILURE;
+    }
+
+    const std::string file_name = argv[1];
+
+    httplib::Client cli("localhost", 8080);
+
+    int length = get_content_length(cli, file_name);
+
     return EXIT_SUCCESS;
 }
